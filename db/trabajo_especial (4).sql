d0e7addce6fa54310860a2159dee1fd544967e87
jaz <reynosojazmin336@gmail.com>
2023-10-13 21:12:22 -0300

    agregue la nueva base de datos

--- db/trabajo_especial (4).sql
+++ db/trabajo_especial (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-10-2023 a las 18:55:30
+-- Tiempo de generación: 14-10-2023 a las 02:11:18
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -12,7 +12,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40s01 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
@@ -34,6 +34,14 @@ CREATE TABLE `companias` (
   `fecha_creacion` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `companias`
+--
+
+INSERT INTO `companias` (`id_empresa`, `nombre`, `cotizacion`, `fecha_creacion`) VALUES
+(1, 'riot', 1000, '2023-10-04'),
+(2, 'epic', 1000, '0000-00-00');
+
 -- --------------------------------------------------------
 
 --
@@ -54,10 +62,19 @@ CREATE TABLE `usuario` (
 
 CREATE TABLE `videojuegos` (
   `id_videojuegos` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
+  `videojuego` varchar(45) NOT NULL,
   `genero` text NOT NULL,
   `id_empresa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `videojuegos`
+--
+
+INSERT INTO `videojuegos` (`id_videojuegos`, `videojuego`, `genero`, `id_empresa`) VALUES
+(1, 'Julian', 'dsa', 1),
+(2, 'lol', 'shoter', 1),
+(3, 'valo', 'tiros', 1);
 
 --
 -- Índices para tablas volcadas
@@ -90,7 +107,7 @@ ALTER TABLE `videojuegos`
 -- AUTO_INCREMENT de la tabla `companias`
 --
 ALTER TABLE `companias`
-  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -102,7 +119,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `videojuegos`
 --
 ALTER TABLE `videojuegos`
-  MODIFY `id_videojuegos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_videojuegos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
